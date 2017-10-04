1cab28782927ac6f43ff898cea83cb6e497b52b6
Mihkel Lukats <mihkel.lukats@gmail.com>
2017-10-04 08:29:56 +0300

    isolation

---
+++ AIND-Isolation/agent_test.py
@@ -0,0 +1,46 @@
+"""This file is provided as a starting template for writing your own unit
+tests to run and debug your minimax and alphabeta agents locally.  The test
+cases used by the project assistant are not public.
+"""
+
+import unittest
+
+import isolation
+import game_agent
+
+from importlib import reload
+
+
+class IsolationTest(unittest.TestCase):
+    """Unit tests for isolation agents"""
+
+    def setUp(self):
+        reload(game_agent)
+        self.player1 = "Player1"
+        self.player2 = "Player2"
+        self.game = isolation.Board(self.player1, self.player2)
+
+    def test_game_play(self):
+        game = isolation.Board(self.player1, self.player2, width=3, height=3)
+        depth = 10000
+        player = game_agent.MinimaxPlayer(search_depth=depth)
+        time_left = lambda: 10000
+        print(game.to_string(), -1)
+
+        for i in range(13):
+            move = player.get_move(game, time_left)
+            if move == (-1, -1):
+                print("active player: {}".format(game.active_player))
+                break
+            game.apply_move(move)
+            print(game.to_string(), i)
+
+        pass
+
+    @unittest.skip
+    def test_return_best_starting_position_if_first_to_act(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
